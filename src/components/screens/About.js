e2acf09337d2fdc443afca8e31b0e0d1fb4b8c06
Alonso Parra <alonsogparra@gmail.com>
2020-06-15 10:38:17 -0400

    About screen update with navigation props added

--- src/components/screens/About.js
+++ src/components/screens/About.js
@@ -3,10 +3,10 @@ import { View } from 'react-native';
 import { Card, Title, Paragraph } from 'react-native-paper';
 import AboutHeader from '../headers/AboutHeader';
 
-export default function About () {
+export default function About ({ navigation }) {
   return (
     <View>
-      <AboutHeader />
+      <AboutHeader navigation={navigation} />
       <Card>
         <Card.Content>
           <Title>Alonso Parra</Title>
