58d58da4ca79b0441fd21a1cbe316c747419ae7f
Alonso Parra <alonsogparra@gmail.com>
2020-06-15 11:01:00 -0400

    SearchUser added to make the searchbar functional, making the dialog functional to get visible with the user info, flatlist built to show users

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -91,13 +91,82 @@ export class Home extends Component {
     );
   }
 
+  searchUser = (value) => {
+    this.setState({ refreshing: false });
+    const filteredUsers = this.state.dataCopy.filter((user) => {
+      let userLowerCase = (
+        user.name.first +
+        " " +
+        user.name.last
+      ).toLowerCase();
+
+      let searchTermLowerCase = value.toLowerCase();
+
+      return userLowerCase.indexOf(searchTermLowerCase) > -1;
+    });
+    this.setState({ data: filteredUsers });
+  };
+
+  hideDialog = () => this.setState({ visible: false });
+
+  showDialog = (item) => {
+    this.setState({ visible: true, itemBackup: item });
+  }
+
+  renderItem = ({ item }) => (
+    <TouchableOpacity
+      onPress={() => this.showDialog(item)}
+    >
+      <Card.Title
+        title= {item.name.first + ' ' + item.name.last}
+        subtitle={item.login.username}
+        left={(props) => <Avatar.Image size={50} source={{uri: item.picture.thumbnail}} />}
+      />
+
+    </TouchableOpacity>
+  );
+
   render() {
 
     const { navigation } = this.props;
+    const { itemBackup, visible} = this.state;
 
     return (
       <View style={{ flex: 1}}>
         <HomeHeader navigation={navigation} />
+        <SafeAreaView />
+        <Searchbar
+          placeholder="Search"
+          onChangeText={(value) => this.searchUser(value)}
+        />
+
+        <Portal>
+          <Dialog
+            visible={visible}
+            onDismiss={this.hideDialog}
+          >
+          <Dialog.Content>
+            <Paragraph>{itemBackup !== null ? itemBackup.name.first + ' ' + itemBackup.name.last : null}</Paragraph>
+          </Dialog.Content>
+          </Dialog>
+        </Portal>
+
+        <FlatList
+          data={this.state.data}
+          renderItem={this.renderItem}
+          keyExtractor={(item) => item.login.uuid}
+          ItemSeparatorComponent={this.renderSeparator}
+          onRefresh={this.handleRefresh}
+          refreshing={this.state.refreshing}
+          onEndReached={() => this.handleLoadMore}
+          onEndReachedThreshold={50}
+          ListEmptyComponent={() => (
+            <View>
+              <Text>No Contacts Found</Text>
+            </View>
+          )}
+        />
+
       </View>
     );
   }
